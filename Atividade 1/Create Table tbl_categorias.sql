4b0a9ad038d4366534d1a627ef99d7c91f30aa12
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2018-02-16 10:57:06 -0200

    Add file Atividade1

---
+++ Atividade 1/Create Table tbl_categorias.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tbl_clientes (
+    tbl_idCategoria int NOT NULL,
+    tbl_categoria varchar(100),
+    tbl_status varchar(50),
+    PRIMARY KEY (tbl_idCategoria)    
+);
---
+++ Atividade 1/Create Table tbl_clientes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tbl_clientes (
+    tbl_idCliente int NOT NULL,
+    tbl_nome varchar(100),
+    tbl_idade int,
+    tbl_telefone varchar(11),
+    tbl_endereco varchar(100),
+    tbl_codCategoria int,
+    PRIMARY KEY (tbl_idCliente)  
+);
